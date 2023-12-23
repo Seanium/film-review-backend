0c5a03f2f5c8ca9cc16c0ea3a8db0a4e33705479
sean <2579935313@qq.com>
2023-12-23 17:20:22 +0800

    feat: 登录成功时返回user的id和token

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='register'),
+    path('login/', LoginAPIView.as_view(), name='login'),
     path('profile/<int:pk>/', UserProfileView.as_view(), name='user-profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,23 @@
+from django.contrib.auth.models import User
 from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import UserProfile
 from .serializers import RegisterSerializer, UserProfileSerializer
+
+
+class LoginAPIView(APIView):
+    '''
+    登录成功时返回user的id和token
+    '''
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = get_object_or_404(User, username=username)
+        if user.check_password(password):
+            return Response({'user_id': user.id, 'token': user.auth_token.key})
+        return Response({'message': '用户名或密码错误'}, status=400)
 
 
 class RegisterAPIView(APIView):
